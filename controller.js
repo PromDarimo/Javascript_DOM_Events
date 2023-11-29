7a2ea337c1d9f48da7fdda318ab11d69169cecb1
Darimo <pozzmo2022@gmail.com>
2023-11-30 01:14:20 +0700

    onMousehoveer and out event

--- controller.js
+++ controller.js
@@ -22,16 +22,16 @@ btn1 = document.getElementById("btn1").onclick = displayDate;
 // onLoad
 function checkCookie() {
     let text = "";
-    if (navigator.cookieEnabled == true) {
+    if (navigator.cookieEnabled == true) { //check is cookie is enable
         text = "Cookies are enabled.";
-    } else {
-        text = "Cookies are not enabled.";
+    } else {                               //id cookie is not enable
+        text = "Cookies are not enabled."; 
     }
     document.getElementById("check-cookie").innerHTML = text;
 
     // onLoad can use for read data also
-    let data = "data.json";
-    fetch(data)
+    let data = "data.json";     //let data = data.json file
+    fetch(data)                 
         .then(x => x.text())
         .then(y => document.getElementById("data").innerHTML = y);
 }
@@ -64,5 +64,18 @@ function onMousedown(){
     txt1 = document.getElementById("mouse-text"); //id from div
     txt1.style.backgroundColor = "gray";
     txt1.innerHTML = "Moon";
+}
 
+// onMousehover
+function onMousehover(){
+    txt1 = document.getElementById("hover-text"); //id from div
+    txt1.style.backgroundColor = "gray";
+    txt1.innerHTML = "Moon";
+}
+
+// onMouseout
+function onMouseout(){
+    txt1 = document.getElementById("hover-text"); //id from div
+    txt1.style.backgroundColor = "orange";
+    txt1.innerHTML = "Sunset";
 }
