e0ba48a83b1b6a9b038d5edb181b3deff32c5c3f
Darimo <pozzmo2022@gmail.com>
2023-11-30 00:13:54 +0700

    event click by id

--- controller.js
+++ controller.js
@@ -9,3 +9,14 @@ function onDisplayText()
 
     console.log(txt1);
 }
+
+// click by id
+
+// Function to display date
+function displayDate(){
+    document.getElementById("demo_date").innerHTML = Date();
+    // Date() is build in in Javascript;
+}
+
+// Declared btn1 with id="btn1" and set event
+btn1 = document.getElementById("btn1").onclick = displayDate;
