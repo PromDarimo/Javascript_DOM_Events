f39ddbdd5bb050b0b5e069973512e01fd751b975
Darimo <pozzmo2022@gmail.com>
2023-11-30 00:31:49 +0700

    onLoad event

--- controller.js
+++ controller.js
@@ -1,5 +1,5 @@
-function onDisplayText()
-{
+// onClick by event
+function onDisplayText() {
     txt1 = document.getElementById("inp1").value;
     demo = document.getElementById("demo").innerHTML = txt1;
     // let demo = txt1
@@ -10,13 +10,28 @@ function onDisplayText()
     console.log(txt1);
 }
 
-// click by id
-
-// Function to display date
-function displayDate(){
+// onClick by id
+function displayDate() { // Function to display date
     document.getElementById("demo_date").innerHTML = Date();
     // Date() is build in in Javascript;
 }
 
 // Declared btn1 with id="btn1" and set event
 btn1 = document.getElementById("btn1").onclick = displayDate;
+
+// onLoad
+function checkCookie() {
+    let text = "";
+    if (navigator.cookieEnabled == true) {
+        text = "Cookies are enabled.";
+    } else {
+        text = "Cookies are not enabled.";
+    }
+    document.getElementById("check-cookie").innerHTML = text;
+
+    // onLoad can use for read data also
+    let data = "data.json";
+    fetch(data)
+        .then(x => x.text())
+        .then(y => document.getElementById("data").innerHTML = y);
+}
